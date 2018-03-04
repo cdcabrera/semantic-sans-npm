75f050b8a544d8b89331a802b1d2b38f74db9728
CD Cabrera <cabrera.code@gmail.com>
2018-03-04 05:25:02 -0500

    perf(build): mod

---
+++ scripts/release.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+require('dotenv').config();
+
+const fs = require('fs');
+const path = require('path');
+const version = require('../package').version;
+const token = process.env.GH_TOKEN;
+const { exec } = require('child_process');
+
+const getChangeLog = () => {
+    let file;
+    let contents = '';
+
+    try {
+        file = path.resolve(__dirname, '../CHANGELOG.md');
+
+        contents = fs.readFileSync(file, 'utf8');
+        contents = contents.replace(/\r\n/g, '\n');
+        contents = contents.split(`<a name="${version}"></a>`)[1];
+
+        fs.unlinkSync(file);
+    } catch(e) {}
+
+    return contents;
+};
+
+const gitHubRelease = () => {
+    const releasesApi = 'https://api.github.com/repos/cdcabrera/semantic-sans-npm/releases';
+
+    const data = {
+        tag_name: `v${version}`,
+        target_commitish: "master",
+        name: `v${version}`,
+        body: contents,
+        draft: false,
+        prerelease: false
+    };
+
+    const execStr = `curl -H "Authorization: token ${token}" -H "Content-Type: application/json" -d '${JSON.stringify(data)}' ${releasesApi}`;
+
+    return exec.execSync(execStr, {stdio:'inherit'});
+};
+
+const log = getChangeLog();
+
+if (log !== '') {
+    gitHubRelease();
+} else {
+    console.log('Changelog not available, exiting.');
+    process.exit();
+}
